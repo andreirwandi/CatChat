446868e7a02f2787e05ea9379d8216fd43edcf30
andreirwandi <andreirwandi1103@yahoo.com>
2021-01-08 14:39:34 +0700

    First Commit

---
+++ app/src/main/java/com/andreirwandi/catchat/HelpActivity.java
@@ -0,0 +1,20 @@
+package com.andreirwandi.catchat;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    //    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
